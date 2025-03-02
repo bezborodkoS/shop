c1925e65fa50481407dda10454f5e16dd29b52f5
bezborodkoS <rotp25@gmail.com>
2025-03-02 20:18:27 +0200

    first setting

---
+++ src/main/resources/table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE category (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         price DECIMAL(10,2) NOT NULL,
+                         category_id INT,
+                         FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE customer (
+                          id SERIAL PRIMARY KEY,
+                          username VARCHAR(255) UNIQUE NOT NULL,
+                          password VARCHAR(255) NOT NULL
+);
